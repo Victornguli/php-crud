ce374eac0e93f9fe7c7c1a97f52a3a0f1fcac4f1
Victor <victornjoseph@gmail.com>
2019-05-16 17:28:43 +0300

    Remove paging.php

--- index.php
+++ index.php
@@ -31,15 +31,65 @@
         <div class="page-header">
             <h1>Read Products</h1>
         </div>
-
         <?php
             include "config/database.php";
-            
-            $page = isset($_GET["page"]) ? $_GET["page"] : 1;
-            $records_per_page = 5;
+            echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create new Product</a>";
 
-            $from_record_num = ($records_per_page * $page) - $records_per_page;
+            if (isset($_GET["page_no"])){
+                $page_no = $_GET["page_no"];
+            } else{
+                $page_no = 1;
+            }
+            $no_of_records_per_page = 5;
+            $offset = ($no_of_records_per_page * $page_no)-$no_of_records_per_page;
+            //echo $offset;
+            $total_pages_query = "SELECT COUNT(*) FROM products";
+            $result = mysqli_query($conn, $total_pages_query);
+            $total_rows = mysqli_fetch_array($result)[0];
+            $total_pages =  ceil($total_rows / $no_of_records_per_page);
+            $query = "SELECT * FROM products LIMIT $offset, $no_of_records_per_page";
+            $res_data = mysqli_query($conn, $query);
 
+            if(mysqli_num_rows($res_data) > 0 ){
+                echo "<table class='table table-hover table-responsive table-bordered'>";//start table
+ 
+                //creating our table heading
+                echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Name</th>";
+                    echo "<th>Description</th>";
+                    echo "<th>Price</th>";
+                    echo "<th>Action</th>";
+                echo "</tr>";
+
+                //Table with records from database
+                while($row = mysqli_fetch_assoc($res_data)){
+                    echo "<tr>";
+                        echo "<td>{$row["id"]}</td>";
+                        echo "<td>{$row["name"]}</td>";
+                        echo "<td>{$row["description"]}</td>";
+                        echo "<td>&#36; {$row["price"]}</td>";
+                        echo "<td>";
+                            //Read one record
+                            echo "<a href='read_one.php?id={$row["id"]}' class='btn btn-info m-r-1em'> Read </a>";
+                            
+                            //Update one record
+                            echo "<a href='update.php?id={$row["id"]}' class='btn btn-primary m-r-1em'> Edit </a>";
+
+                            //Delete User
+                            echo "<a href='#' onclick='deleteUser({$row["id"]})' class='btn btn-danger m-r-1em'> Delete </a>";
+                        echo "</td>";
+                    echo "</tr>";
+
+                }
+                // end table
+                echo "</table>";
+        }else{
+            echo mysqli_error($conn)."<div class='alert alert-danger alert-dismissible'>
+            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+            <strong>Warning!</strong> Zero records found.
+        </div>";
+        }
 
             $action = isset($_GET["action"]) ? $_GET["action"] : "";
             
@@ -51,73 +101,23 @@
             }
 
 
-            $query = "SELECT id,name,description,price,created,modified FROM products ORDER BY id DESC
-                LIMIT :from_record_num, :record_per_page";
+            //$query = "SELECT id,name,description,price,created,modified FROM products ORDER BY id DESC LIMIT $offest, $no";
+
+            //$result = mysqli_query($conn, $query);
             
-            $stmt = $conn->prepare($query);
-            $stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
-            $stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
-            $stmt->execute();
-
-            $result = mysqli_query($conn, $query);
-            
-            echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create new Product</a>";
-
-            if(mysqli_num_rows($result) > 0){
-                    //Data
-                    echo "<table class='table table-hover table-responsive table-bordered'>";//start table
- 
-                    //creating our table heading
-                    echo "<tr>";
-                        echo "<th>ID</th>";
-                        echo "<th>Name</th>";
-                        echo "<th>Description</th>";
-                        echo "<th>Price</th>";
-                        echo "<th>Action</th>";
-                    echo "</tr>";
-
-                    //Table with records from database
-                    while($row = mysqli_fetch_assoc($result)){
-                        echo "<tr>";
-                            echo "<td>{$row["id"]}</td>";
-                            echo "<td>{$row["name"]}</td>";
-                            echo "<td>{$row["description"]}</td>";
-                            echo "<td>&#36; {$row["price"]}</td>";
-                            echo "<td>";
-                                //Read one record
-                                echo "<a href='read_one.php?id={$row["id"]}' class='btn btn-info m-r-1em'> Read </a>";
-                                
-                                //Update one record
-                                echo "<a href='update.php?id={$row["id"]}' class='btn btn-primary m-r-1em'> Edit </a>";
-
-                                //Delete User
-                                echo "<a href='#' onclick='deleteUser({$row["id"]})' class='btn btn-danger m-r-1em'> Delete </a>";
-                            echo "</td>";
-                        echo "</tr>";
-
-                    }
-                    $query = "SELECT COUNT(*) as total_rows FROM products";
-                    $stmt = $con->prepare($query);
-                    
-                    // execute query
-                    $stmt->execute();
-                    
-                    // get total rows
-                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                    $total_rows = $row['total_rows'];
-                    // end table
-                    echo "</table>";
-            }else{
-                echo "<div class='alert alert-danger alert-dismissible'>
-                <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                <strong>Warning!</strong> Zero records found.
-            </div>";
-            }
-
             // paginate records
-            $page_url="index.php?";
-            include_once "paging.php";
          ?>
+        
+        <ul class="pagination">
+            <li> <a href="?page_no=1">First</a> </li>
+            <li class="<?php  if($page_no <= 1) {echo 'disabled';} ?>">
+                <a href="<?php if($page_no <= 1){echo '#';} else {echo '?page_no='.($page_no-1);} ?>">Prev</a>
+            </li>
+            <li class="<?php if($page_no >= $total_pages){ echo 'disabled'; } ?>">
+                <a href="<?php if($page_no >= $total_pages){ echo '#'; } else { echo '?page_no='.($page_no+1);} ?>">Next</a>
+            </li>
+            <li><a href="?page_no=<?php echo $total_pages; ?>">Last</a></li>
+        </ul>
     </div>
     <!-- End container -->
 
